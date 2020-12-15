4a55be7b3e9c6d14b819da271f2583752c7538e1
Zuzana Kormancova <zkormancova@MacBook-Pro.nutanix.lab>
2020-12-15 12:44:39 +0000

    added new game

---
+++ basic-Tic-Tac-Toe/app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const playerDisplay = document.querySelector("#player");
+  let currentPlayer = "playerX";
+
+  squares.forEach((square) => {
+    square.addEventListener("click", clickOutcome);
+  });
+
+  function clickOutcome(e) {
+    const squareArray = Array.from(squares);
+    const index = squareArray.indexOf(e.target);
+    console.log(index);
+    playerDisplay.innerHTML = currentPlayer;
+
+    if (currentPlayer === "playerX") {
+      squares[index].classList.add("playerX");
+      currentPlayer = "playerO";
+    } else {
+      squares[index].classList.add("playerO");
+      currentPlayer = "playerX";
+    }
+  }
+});
